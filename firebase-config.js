84ed13c7b5a955d8f022fdf479c858246e5bb129
SoralDaBest <usingforprodigyonly@gmail.com>
2025-01-10 14:12:46 -0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDsqcVJDGpVPXI8-6ZJcqYwCR9Ejpw43lQ",
+    authDomain: "play-bliss.firebaseapp.com",
+    databaseURL: "https://play-bliss-default-rtdb.firebaseio.com",
+    projectId: "play-bliss",
+    storageBucket: "play-bliss.appspot.com",
+    messagingSenderId: "605761107676",
+    appId: "1:605761107676:web:a5391f6f614f27c27ae619",
+    measurementId: "G-PQMXK8WQKB"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
